355aa863e067a699ae4da85e18b6f7052c1477e6
NazarKulyk <156192686+NazarKulyk6@users.noreply.github.com>
2026-03-01 23:32:34 +0100

    Add movie parameter to cinema_visit function

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.people.customer import Customer
 
 
 def cinema_visit(
-    movie: str,
     customers: list,
     hall_number: int,
     cleaner: str,
+    movie: str,
 ) -> None:
     customer_list = [
         Customer(name=c["name"], food=c["food"]) for c in customers
